8359d0d55a951af7b89f24c7069f750fd3ab4eae
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2023-12-13 14:32:00 +0530

    area of a square

---
+++ area_of_square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float s,area;
+    printf("Enter the size of the square: ");
+    scanf("%f",&s);
+    area=s*s;
+    printf("Area  of the square: %f",area);
+    return 0;
+}
